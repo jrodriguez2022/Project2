308938369a586822aad55c8593ec4130a454f9c2
Jeff <jeff.rodriguez@ngc.com>
2022-03-20 01:22:05 +0000

    initial

---
+++ project2.js
@@ -0,0 +1,25 @@
+const http = require("http");
+
+const host = '137.184.239.127';
+const port = 8000;
+
+const requestListener = function (req, res) {
+
+res.setHeader("Content-Type", "text/html");
+res.writeHead(200);
+res.write('<html><body>');
+res.write('<h1>This is Project2</h1><br/><br/>/Hello = Greatings <br/>');
+switch (req.url) {
+	case "/hello":
+	   url=req.url;
+	   res.write("The URL you used was: "+url+"<br/><br/><br/> <h1>HELLO!!</h1>");
+	   break;
+}
+
+res.end('</body></html>');
+};
+
+const server = http.createServer(requestListener);
+server.listen(port, host, () => {
+    console.log('Server is running on http://'+host+':'+port);
+});
